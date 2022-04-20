89cfbf5559abb011de520621d8c5bdb11c28a754
egiptipavel <kinqstone585@gmail.com>
2022-04-20 20:01:50 +0300

    Refactor aggressive mob

--- src/entities/aggressive_enemy.py
+++ src/entities/aggressive_enemy.py
@@ -13,6 +13,15 @@ class AggressiveEnemy(Enemy):
         super().__init__(health, max_health, cell_pos, image_key, damage, exp_gain)
         self.attack_radius = attack_radius
 
+    def __try_move__(self, num_steps: int, pos: Tuple[int, int], enemies: List[Enemy],
+                     cells: Dict[Tuple[int, int], Cell]) -> bool:
+        if num_steps > self.attack_radius or pos not in cells or cells[pos].cell_type == CellType.Wall:
+            return False
+        for enemy in enemies:
+            if enemy.cell_pos[0] == pos[0] and enemy.cell_pos[1] == pos[1]:
+                return False
+        return True
+
     def __get_next_pos__(self,
                          pos: Tuple[int, int],
                          hero_pos: Tuple[int, int],
@@ -20,36 +29,39 @@ class AggressiveEnemy(Enemy):
                          enemies: List[Enemy],
                          cells: Dict[Tuple[int, int], Cell],
                          first_step: Optional[Tuple[int, int]]) -> Optional[Tuple[Tuple[int, int], int]]:
-        if num_steps > self.attack_radius or pos not in cells or cells[pos].cell_type == CellType.Wall:
+        if not self.__try_move__(num_steps, pos, enemies, cells):
             return None
-        for enemy in enemies:
-            if enemy.cell_pos[0] == pos[0] and enemy.cell_pos[1] == pos[1]:
-                return None
         if pos[0] == hero_pos[0] and pos[1] == hero_pos[1]:
             return first_step, num_steps
-        step_left = self.__get_next_pos__((pos[0] - 1, pos[1]), hero_pos, num_steps + 1, enemies, cells,
-                                          first_step if first_step is not None else (pos[0] - 1, pos[1]))
-        step_right = self.__get_next_pos__((pos[0] + 1, pos[1]), hero_pos, num_steps + 1, enemies, cells,
-                                           first_step if first_step is not None else (pos[0] + 1, pos[1]))
-        step_up = self.__get_next_pos__((pos[0], pos[1] - 1), hero_pos, num_steps + 1, enemies, cells,
-                                        first_step if first_step is not None else (pos[0], pos[1] - 1))
-        step_down = self.__get_next_pos__((pos[0], pos[1] + 1), hero_pos, num_steps + 1, enemies, cells,
-                                          first_step if first_step is not None else (pos[0], pos[1] + 1))
+        potential_positions = [
+            (pos[0] - 1, pos[1]), (pos[0] + 1, pos[1]),
+            (pos[0], pos[1] - 1), (pos[0], pos[1] + 1)
+        ]
+        if first_step is None:
+            results_of_steps = [
+                self.__get_next_pos__(pot_pos, hero_pos, num_steps + 1, enemies, cells, pot_pos)
+                for pot_pos in potential_positions
+            ]
+        else:
+            results_of_steps = [
+                self.__get_next_pos__(pot_pos, hero_pos, num_steps + 1, enemies, cells, first_step)
+                for pot_pos in potential_positions
+            ]
         steps = sorted(
-            list(filter(lambda s: s is not None, [step_left, step_right, step_up, step_down])),
-            key=lambda s: s[1])
-        if not steps:
-            return None
-        return steps[0]
+            list(filter(lambda step: step is not None, results_of_steps)),
+            key=lambda s: s[1]
+        )
+        return steps[0] if steps else None
 
     def move(self, hero, enemies, cells):
         close_enemies = []
         for enemy in enemies:
-            if enemy.cell_pos[0] != self.cell_pos[0] and enemy.cell_pos[1] != self.cell_pos[1] and \
-                    __get_dist__(self.cell_pos, enemy.cell_pos) <= self.attack_radius:
+            dist = __get_dist__(self.cell_pos, enemy.cell_pos)
+            if 0 < dist <= self.attack_radius:
                 close_enemies.append(enemy)
-        next_step = \
-            self.__get_next_pos__((self.cell_pos[0], self.cell_pos[1]), hero.cell_pos, 0, close_enemies, cells, None)
+        next_step = self.__get_next_pos__(
+            (self.cell_pos[0], self.cell_pos[1]), hero.cell_pos, 0, close_enemies, cells, None
+        )
         if next_step is not None:
             next_pos = next_step[0]
             if next_pos[0] == hero.cell_pos[0] and next_pos[1] == hero.cell_pos[1]:
